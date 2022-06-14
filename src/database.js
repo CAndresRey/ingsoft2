98da694e93c05d6326e72fa244f215d8cb7ef068
Carlos Andres Lozano Rey <andres.lozano@hunty.com>
2022-06-14 10:46:02 -0500

    updated

--- src/database.js
+++ src/database.js
@@ -1,1 +1,9 @@
-print('do')
+const mongoose = require('mongoose');
+
+mongoose.set('useFindAndModify', false);
+mongoose.connect('mongodb://localhost/node-notes-db', {
+  useCreateIndex: true,
+  useNewUrlParser: true
+})
+  .then(db => console.log('DB is connected'))
+  .catch(err => console.error(err));
